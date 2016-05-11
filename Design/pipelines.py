bdf9000d5df0df85c910af23d99e4d6d8f0fd731
gryphon <2314668686@qq.com>
2016-05-11 23:14:43 +0800

    update

--- Design/pipelines.py
+++ Design/pipelines.py
@@ -10,44 +10,48 @@ import pymysql
 '''
 	格式化当前时间
 '''
+
+
 def getCurrentTime():
-	# time.strftime('%Y-%m-%d %H:%M:%S',time.localtime(time.time()))
-	return datetime.datetime.strftime(datetime.datetime.now(),'%Y-%m-%d %H:%M:%S')
+    # time.strftime('%Y-%m-%d %H:%M:%S',time.localtime(time.time()))
+    return datetime.datetime.strftime(datetime.datetime.now(), '%Y-%m-%d %H:%M:%S')
+
 
 def dbHandle():
-	conn = pymysql.connect(
-		host='10.0.1.112',
-		user='root',
-		passwd='',
-		db='design',
-		charset='utf8',
-		use_unicode= False
-	)
-	return conn
+    conn = pymysql.connect(
+        # host='10.0.1.112',
+        host='localhost',
+        user='root',
+        passwd='root',
+        db='design',
+        charset='utf8',
+        use_unicode=False
+    )
+    return conn
+
 
 class DesignPipeline(object):
+    def process_item(self, item, spider):
+        dbObject = dbHandle()
+        cursor = dbObject.cursor()
+        sql = "insert into design.image (title,icon,img,tags,create_time,modify_time) values (%s,%s,%s,%s,%s,%s)"
 
-	def process_item(self, item, spider):
-		dbObject = dbHandle()
-		cursor = dbObject.cursor()
-		sql = "insert into design.image (title,icon,img,tags,create_time,modify_time) values (%s,%s,%s,%s,%s,%s)"
+        images = ''
+        if item['img']:
+            for image in item['img']:
+                images += image + ','
 
-		images = ''
-		if item['img']:
-			for image in item['img']:
-				images += image + ','
+        tags = ''
+        if item['tags']:
+            for tag in item['tags']:
+                tags += tag + ','
 
-		tags = ''
-		if item['tags']:
-			for tag in item['tags']:
-				tags += tag + ','
+        icon = item['icon']
 
-		icon = item['icon']
+        selectSql = 'SELECT id FROM image WHERE icon = "%s"' % icon
+        result = cursor.execute(selectSql)
 
-		selectSql = 'SELECT id FROM image WHERE icon = "%s"' % icon
-		result = cursor.execute(selectSql)
-
-		if result == 0:
-			cursor.execute(sql, (item['title'], icon, images,tags,getCurrentTime(), getCurrentTime()))
-			dbObject.commit()
-		return item
+        if result == 0:
+            cursor.execute(sql, (item['title'], icon, images, tags, getCurrentTime(), getCurrentTime()))
+            dbObject.commit()
+        return item
--- Design/spiders/Spider_freeimages.py
+++ Design/spiders/Spider_freeimages.py
@@ -1,40 +1,99 @@
 #-*- coding:utf-8 -*-
 import scrapy
+from Design.items import DesignItem
 
 class Spider_freeimages(scrapy.Spider):
-	name = 'freeimages'
+    name = 'freeimages'
 
-	start_urls = [
-		'http://cn.freeimages.com/image'
-	]
+    start_urls = [
+        'http://cn.freeimages.com/image',
+    ]
 
-	baseUrl = 'http://cn.freeimages.com'
+    baseUrl = 'http://cn.freeimages.com'
 
-	def parse(self, response):
-		for category in response.xpath('//ul[@class="category-group"]/li'):
-			imageType = category.xpath('./a/@href').extract()
-			if imageType:
-				imageType = imageType[0]
-				if imageType != '/image':
-					url = self.baseUrl + imageType
+    def parse(self, response):
+        for category in response.xpath('//ul[@class="category-group"]/li'):
+            imageType = category.xpath('./a/@href').extract()
+            if imageType:
+                imageType = imageType[0]
+                if imageType != '/image':
+                    url = self.baseUrl + imageType
 
-					tag = category.xpath('./a/text()').extract()
-					if tag:
-						tag = tag[0]
-						print tag
-						yield scrapy.Request(url,callback=self.parseItem,meta={'tag':tag})
-			break
+                    tag = category.xpath('./a/text()').extract()
+                    if tag:
+                        tag = tag[0]
+                        print tag
+                        yield scrapy.Request(url, callback=self.parseItem, meta={'tag': tag})
 
-	def parseItem(self,response):
-		tag = response.meta['tag']
+    def parseItem(self, response):
+        tag = response.meta['tag']
 
-		for item in response.xpath('ul[@class="thumb-premium fn-clear"]/li'):
-			icon = item.xpath('./a/span/span/img/@src').extract()
-			if icon:
-				icon = icon[0]
+        for item in response.xpath('//ul[@class="thumb-premium fn-clear"]/li'):
+            design = DesignItem()
 
-				detailUrl = item.xpath('./a/@href').extract()
-				if detailUrl:
-					detailUrl = detailUrl[0]
+            icon = item.xpath('./a/span/span/img/@src').extract()
+            if icon:
+                icon = icon[0]
+                design['icon'] = icon
+            else:
+                design['icon'] = ''
 
-					
+            design['title'] = ''
+            design['img'] = []
+            design['tags'] = tag
+            yield design
+
+        for page in response.xpath('//div[@class="pagination fn-right"]/a'):
+            nextPage = page.xpath('./text()').extract()
+            if nextPage:
+                nextPage = nextPage[0]
+
+                if nextPage == 'Next'.decode('utf-8'):
+                    url = page.xpath('./@href').extract()
+                    if url:
+                        url = url[0]
+                        yield scrapy.Request(url,callback=self.parseItem,meta={'tag': tag})
+
+        # for item in response.xpath('//ul[@class="thumb-group thumb-group160x160 thumb-group-fixed thumb-group-line fn-clear"]/li'):
+        #     image = DesignItem()
+        #
+        #     title = item.xpath('./a/text()').extract()
+        #     if title:
+        #         title = title[0]
+        #         image['title'] = title
+        #     else:
+        #         image['title'] = ''
+        #
+        #
+        #     icon = item.xpath('./a/span[@class="thumb-img offset-preview"]/img/@src').extract()
+        #     if icon:
+        #         icon = icon[0]
+        #         image['icon'] = icon
+        #     else:
+        #         image['icon'] = ''
+        #
+        #     image['tags'] = ''
+        #
+        #     detailUrl = item.xpath('./a/@href').extract()
+        #     if detailUrl:
+        #         detailUrl = self.baseUrl + detailUrl[0]
+        #         yield scrapy.Request(detailUrl,callback=self.parseDetail,meta={'design':image})
+
+        #     for page in response.xpath('//div[@class="pagination fn-right"]/a'):
+        #         nextPage = page.xpath('./text()').extract()
+        #         if nextPage:
+        #             nextPage = nextPage[0]
+        #
+        #             if nextPage == 'Next'.decode('utf-8'):
+        #                 url = page.xpath('./@href').extract()
+        #                 if url:
+        #                     url = url[0]
+        #                     yield scrapy.Request(url, callback=self.parseItem, meta={'tag': tag})
+
+    def parseDetail(self,response):
+        design = response.meta['design']
+        img = response.xpath('//div[@class="img-preview-inner"]/a[@class="preview cboxElement"]/img/@src')
+        if img:
+            img = img[0]
+            design['img'] = img
+            yield design
